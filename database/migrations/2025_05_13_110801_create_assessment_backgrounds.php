f3470d7cdff769527e351aa82a5d16e626da0f7c
prasadt <prasadt@kensium.com>
2025-05-13 19:22:43 +0530

    Implementation and Resolve Assessment BacKGround Changes

--- database/migrations/2025_05_13_110801_create_assessment_backgrounds.php
+++ database/migrations/2025_05_13_110801_create_assessment_backgrounds.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('assessment_background', function (Blueprint $table) {
+        Schema::create('assessment_backgrounds', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('description')->nullable();
@@ -25,6 +25,6 @@ return new class extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('assessment_background');
+        Schema::dropIfExists('assessment_backgrounds');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -36,10 +36,19 @@ Route::middleware(['auth'])->group(function () {
     Route::get('assessment', [AssessmentController::class, 'index'])->name('assessment.index');
     Route::get('assessment/create', [AssessmentController::class, 'create'])->name('assessment.create');
     Route::post('assessment', [AssessmentController::class, 'store'])->name('assessment.store');
-    Route::get('assessment/{assessment}', [AssessmentController::class, 'show'])->name('assessment.show');
-    Route::get('assessment/{assessment}/edit', [AssessmentController::class, 'edit'])->name('assessment.edit');
-    Route::put('assessment/{assessment}', [AssessmentController::class, 'update'])->name('assessment.update');
-    Route::delete('assessment/{assessment}', [AssessmentController::class, 'destroy'])->name('assessment.destroy');
+
+    Route::get('/assessment/start', [AssessmentController::class, 'showAllQuestions'])->name('assessment.start');
+    Route::post('/assessment/submit-all', [AssessmentController::class, 'storeAllAnswers'])->name('assessment.submit');
+
+    Route::get('/assessment/question/{questionId}', [AssessmentController::class, 'showQuestion'])->name('assessment.question');
+    Route::post('/assessment/answer/{questionId}', [AssessmentController::class, 'storeAnswer']);
+    Route::get('/assessment/submit', [AssessmentController::class, 'submit'])->name('assessment.submit');
+    
+    //Route::get('assessment/{assessment}', [AssessmentController::class, 'show'])->name('assessment.show');
+
+    //Route::get('assessment/{assessment}/edit', [AssessmentController::class, 'edit'])->name('assessment.edit');
+    //Route::put('assessment/{assessment}', [AssessmentController::class, 'update'])->name('assessment.update');
+    //Route::delete('assessment/{assessment}', [AssessmentController::class, 'destroy'])->name('assessment.destroy');
 
     // Password Reset Routes for authenticated users
     Route::get('password/reset-form', [UserAuthController::class, 'showResetForm'])->name('password.reset.form');
@@ -161,13 +170,6 @@ Route::get('/test', function () {
 })->name('test');
 
 
-
-Route::get('/assessment/question/{questionId}', [AssessmentController::class, 'showQuestion'])->name('assessment.question');
-Route::post('/assessment/answer/{questionId}', [AssessmentController::class, 'storeAnswer']);
-Route::get('/assessment/submit', [AssessmentController::class, 'submit'])->name('assessment.submit');
-
-Route::get('/assessment/start', [AssessmentController::class, 'showAllQuestions'])->name('assessment.start');
-Route::post('/assessment/submit-all', [AssessmentController::class, 'storeAllAnswers'])->name('assessment.submit');
 
 
 
