09c6cdfceab2a6c0547f8960e1246e707fc08d4d
prakash <prakashr@kensium.com>
2025-06-18 18:49:36 +0530

    add the favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-     {{--<link rel="shortcut icon" href="/images/favicon.png">--}}
+     <link rel="shortcut icon" href="/images/favicon.png">
     <title>E-commerce Scorecard</title>
     @viteReactRefresh
     @vite('resources/js/app.jsx')
